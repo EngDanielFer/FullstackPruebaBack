1da16d6479494cce8c9a31bd042ec8f4ce0533bf
EngDanielFer <dfernando.reyes@gmail.com>
2025-06-03 22:56:14 -0500

    Commit

--- app.py
+++ app.py
@@ -80,6 +80,4 @@ def delete_auto(id_auto):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=port)
